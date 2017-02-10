11e5e4871c362b698cfbe8a9f2e268f66da74160
Mario Geiger <geiger.mario@gmail.com>
2017-02-10 11:07:15 +0100

    hungarian :-)

---
+++ index.js
@@ -0,0 +1,418 @@
+function parse(text) {
+    errors = [];
+
+    ch = text.length === 0 ? null : text[0];
+    k = 0;
+    l = 0;
+    c = 0;
+
+    vmin = null;
+    vmax = null;
+    wishes = [];
+
+    vmin_sum = 0;
+    vmax_sum = 0;
+
+    function eat() {
+        if (ch === "\n") {
+            l++;
+            c = 0;
+        } else {
+            c++;
+        }
+        k++;
+        if (k < text.length) {
+            ch = text[k];
+        } else {
+            ch = null;
+        }
+    }
+
+    function eat_line() {
+        while (is_space(ch)) {
+            eat();
+        }
+        if (ch === "\n") {
+            eat();
+            return;
+        }
+        if (ch === "#") {
+            skip_line();
+            return;
+        }
+        if (ch !== null) {
+            eat_row();
+        }
+    }
+
+    function eat_row() {
+        if (!is_digit(ch)) {
+            cc = c;
+            skip_line();
+            errors.push({
+                from: CodeMirror.Pos(l, cc),
+                to: CodeMirror.Pos(l, c),
+                message: "A row must begin with a number"
+            });
+            return;
+        }
+
+        row = [];
+        x = eat_number();
+        if (isNaN(x)) {
+            skip_line();
+            return;
+        }
+        row.push(x);
+        while (is_space(ch)) {
+            eat();
+        }
+        while (ch === ",") {
+            cc = c;
+            eat(); // eat the comma
+            while (is_space(ch)) {
+                eat();
+            }
+
+            if (!is_digit(ch)) {
+                errors.push({
+                    from: CodeMirror.Pos(l, cc),
+                    to: CodeMirror.Pos(l, c),
+                    message: "After a comma, a number is expected"
+                });
+                skip_line();
+                return;
+            }
+
+            x = eat_number();
+            if (isNaN(x)) {
+                skip_line();
+                return;
+            }
+            row.push(x);
+            while (is_space(ch)) {
+                eat();
+            }
+        }
+
+        if (ch === "#") {
+            skip_line();
+        }
+        if (ch !== "\n" && ch !== null) {
+            cc = c;
+            skip_line();
+            errors.push({
+                from: CodeMirror.Pos(l, cc),
+                to: CodeMirror.Pos(l, c),
+                message: "This is not a valid content"
+            });
+            return;
+        }
+
+        if (vmin !== null && vmin.length !== row.length) {
+            errors.push({
+                from: CodeMirror.Pos(l, 0),
+                to: CodeMirror.Pos(l, c),
+                message: "All the lines must contain the same amount of values"
+            });
+            return;
+        }
+        if (vmin === null) {
+            vmin = row;
+            vmin_sum = 0;
+            for (i = 0; i < vmin.length; ++i) {
+                vmin_sum += vmin[i];
+            }
+        } else if (vmax === null) {
+            vmax = row;
+            vmax_sum = 0;
+            for (i = 0; i < vmax.length; ++i) {
+                vmax_sum += vmax[i];
+            }
+            for (i = 0; i < vmin.length; ++i) {
+                if (vmax[i] < vmin[i]) {
+                    errors.push({
+                        from: CodeMirror.Pos(l, 0),
+                        to: CodeMirror.Pos(l, c),
+                        message: "vmax must be greater or equal than vmin"
+                    });
+                }
+            }
+        } else {
+            wishes.push(row);
+            if (wishes.length > vmax_sum) {
+                errors.push({
+                    from: CodeMirror.Pos(l, 0),
+                    to: CodeMirror.Pos(l, c),
+                    message: "vmax is too small or there is too much entries"
+                });
+            }
+            tmp = row.slice();
+            tmp.sort();
+            for (i = 0; i < tmp.length; ++i) {
+                if (tmp[i] > i) {
+                    errors.push({
+                        from: CodeMirror.Pos(l, 0),
+                        to: CodeMirror.Pos(l, c),
+                        message: "This wish is not fair"
+                    });
+                    break;
+                }
+            }
+        }
+    }
+
+    function is_space(ch) {
+        return ch === " " || ch === "\t";
+    }
+
+    function is_digit(ch) {
+        return ch !== null && (ch >= "0" && ch <= "9");
+    }
+
+    function eat_number() {
+        entry = ch;
+        eat();
+        while (is_digit(ch)) {
+            entry += ch;
+            eat();
+        }
+        num = Number(entry);
+        if (isNaN(num) || num < 0 || entry[0] === "\n") {
+            errors.push({
+                from: CodeMirror.Pos(l, c - entry.length),
+                to: CodeMirror.Pos(l, c),
+                message: "'" + entry + "' is not a non-negative number"
+            });
+            return NaN;
+        }
+        return num;
+    }
+
+    function skip_line() {
+        while (ch !== "\n" && ch !== null) {
+            eat();
+        }
+    }
+
+    while (ch !== null) {
+        eat_line();
+    }
+
+    if (wishes.length < vmin_sum) {
+        errors.push({
+            from: CodeMirror.Pos(0, 0),
+            to: CodeMirror.Pos(l, c),
+            message: "vmin is too high for the amount of entries"
+        });
+    }
+
+    res = {
+        'vmin': vmin,
+        'vmax': vmax,
+        'wishes': wishes
+    };
+
+    return [res, errors];
+}
+
+CodeMirror.defineMode("wish_input", function() {
+    return {
+        startState: function() {
+            return {
+                commentLine: false
+            };
+        },
+        token: function(stream, state) {
+            if (stream.sol()) {
+                state.commentLine = false;
+            }
+            var ch = stream.next().toString();
+            if (state.commentLine) {
+                return "comment";
+            }
+            if (ch === "#") {
+                state.commentLine = true;
+                return "comment";
+            }
+            if (ch === ",") {
+                return "keyword";
+            }
+            return "atom";
+        }
+    };
+});
+
+CodeMirror.registerHelper("lint", "wish_input", function(text) {
+    return parse(text)[1];
+});
+
+function action(wishes, results) {
+    score = 0;
+    for (i = 0; i < wishes.length; ++i) {
+        score += Math.pow(wishes[i][results[i]], 2);
+    }
+    return score;
+}
+
+Module = null; // Global application object.
+statusText = 'NO-STATUS';
+inputCode = null;
+outputCode = null;
+data = null;
+call_time = 0;
+
+// Indicate load success.
+function moduleDidLoad() {
+    Module = document.getElementById('nacl_module');
+    updateStatus('READY');
+}
+
+function button_pressed() {
+    text = inputCode.getValue();
+    out = parse(text);
+    if (out[1].length === 0) {
+        data = out[0];
+        vmin = out[0].vmin;
+        vmax = out[0].vmax;
+        wishes = out[0].wishes;
+
+        vmin_tot = 0;
+        for (i = 0; i < vmin.length; ++i) {
+            vmin_tot += vmin[i];
+        }
+        vmax_tot = 0;
+        for (i = 0; i < vmax.length; ++i) {
+            vmax_tot += vmax[i];
+        }
+
+        cost = [];
+        x = Math.pow(vmin.length, 2);
+
+        for (i = 0; i < wishes.length; ++i) {
+            row = [];
+            for (j = 0; j < vmin.length; ++j) {
+                c = Math.pow(wishes[i][j], 2);
+                for (k = 0; k < vmin[j]; ++k) {
+                    row.push(c);
+                }
+                for (k = vmin[j]; k < vmax[j]; ++k) {
+                    row.push(x + c);
+                }
+            }
+            cost.push(row);
+        }
+
+        // append virtual studdent at the end
+        for (i = wishes.length; i < vmax_tot; ++i) {
+            row = [];
+            for (j = 0; j < vmin.length; ++j) {
+                for (k = 0; k < vmin[j]; ++k) {
+                    row.push(x);
+                }
+                for (k = vmin[j]; k < vmax[j]; ++k) {
+                    row.push(0);
+                }
+            }
+            cost.push(row);
+        }
+
+        Module.postMessage(cost);
+        call_time = new Date().getTime();
+    }
+}
+
+function handleMessage(message_event) {
+    result = message_event.data;
+
+    wishes = data.wishes;
+    vmin = data.vmin;
+    vmax = data.vmax;
+    result.length = wishes.length;
+    for (i = 0; i < result.length; ++i) {
+        for (j = 0; j < vmax.length; ++j) {
+            if (result[i] >= vmax[j]) {
+                result[i] -= vmax[j];
+            } else {
+                result[i] = j;
+                break;
+            }
+        }
+    }
+
+    score = action(wishes, result);
+
+    text = "# attribution,score\n";
+    for (i = 0; i < result.length; ++i) {
+        text += result[i] + "," + wishes[i][result[i]] + "\n";
+    }
+    outputCode.setValue(text);
+
+    var info = document.getElementById('info');
+    info.value = "";
+
+    var dt = (new Date().getTime() - call_time) / 1000;
+    info.value += dt + " seconds.\n";
+    info.value += "Total score of " + score + ".\n\n";
+
+    choices = [];
+    for (i = 0; i < vmin.length; ++i) {
+        choices.push(0);
+    }
+    for (i = 0; i < result.length; ++i) {
+        choices[wishes[i][result[i]]]++;
+    }
+    info.value += "Satisfaction distribution : " + choices.join(" ") + "\n\n";
+
+    v = [];
+    for (i = 0; i < vmin.length; ++i) {
+        v.push(0);
+    }
+    for (i = 0; i < result.length; ++i) {
+        v[result[i]]++;
+    }
+
+    text = "";
+    for (i = 0; i < vmin.length; ++i) {
+        choices = [];
+        for (j = 0; j < vmin.length; ++j) {
+            choices.push(0);
+        }
+
+        for (j = 0; j < result.length; ++j) {
+            if (result[j] === i) {
+                choices[wishes[j][i]]++;
+            }
+        }
+        text += "workshop " + i + ": " + v[i] + " : " + choices.join(" ") + "\n";
+    }
+    info.value += text;
+}
+
+function pageDidLoad() {
+    if (Module === null) {
+        updateStatus('LOADING...');
+    } else {
+        updateStatus();
+    }
+
+    inputCode = CodeMirror.fromTextArea(document.getElementById('input'), {
+        lineNumbers: true,
+        mode: "wish_input",
+        gutters: ["CodeMirror-lint-markers"],
+        lint: true
+    });
+    outputCode = CodeMirror.fromTextArea(document.getElementById('output'), {
+        lineNumbers: true,
+        readOnly: true
+    });
+}
+
+function updateStatus(opt_message) {
+    if (opt_message)
+        statusText = opt_message;
+    var statusField = document.getElementById('statusField');
+    if (statusField) {
+        statusField.innerHTML = statusText;
+    }
+}
--- nacl.cc
+++ nacl.cc
@@ -4,177 +4,48 @@
 #include "ppapi/cpp/var_array.h"
 #include "ppapi/cpp/var_dictionary.h"
 
-#include <algorithm>
-#include <random>
-#include <vector>
+#include "hung.c"
 
 using namespace std;
 
-inline std::mt19937_64 &global_random_engine() {
-    static std::random_device rdev;
-    static std::mt19937_64 eng(rdev());
-    return eng;
-}
-
-inline double canonical() {
-    return std::generate_canonical<double, std::numeric_limits<double>::digits>(
-        global_random_engine());
-}
-
-bool is_not_null(int x) {
-    return x != 0;
-}
-
 class MyInstance : public pp::Instance {
-private:
-size_t g_random_numbers_counter = 0;
-size_t g_random_numbers_size = 64 * 1024;
-vector<double> g_random_numbers;
-
 public:
 explicit MyInstance(PP_Instance instance) : pp::Instance(instance)
 {
-    canonical_fast_initialize();
 }
 virtual ~MyInstance() {
 }
 
-void canonical_fast_initialize() {
-    g_random_numbers_counter = 0;
-    g_random_numbers.resize(g_random_numbers_size);
-    for (size_t i = 0; i < g_random_numbers_size; ++i)
-        g_random_numbers[i] = canonical();
-}
-
-inline double canonical_fast() {
-    return g_random_numbers[g_random_numbers_counter++ % g_random_numbers_size];
-}
-
-size_t min_pos(const vector<double>& xs) {
-    size_t k = 0;
-    double min = xs[0];
-    for (size_t i = 1; i < xs.size(); ++i) {
-        if (xs[i] < min) {
-            min = xs[i];
-            k = i;
-        }
-    }
-    return k;
-}
-
-bool count(const vector<int>& vmin, const vector<int>& vmax, vector<int> &x, const vector<vector<double>> &values) {
-    for (size_t j = 0; j < vmin.size(); ++j) {
-        x[j] = 0;
-    }
-
-    // occupation of each workshop
-    for (size_t i = 0; i < values.size(); ++i) {
-        size_t k = min_pos(values[i]);
-        x[k]++;
-    }
-
-    bool ok = true;
-    for (size_t i = 0; i < x.size(); ++i) {
-        if (x[i] < vmin[i]) {
-            x[i] -= vmin[i];      // negative value for a lack
-            ok = false;
-        } else if (x[i] > vmax[i]) {
-            x[i] -= vmax[i];      // positive value for an overage
-            ok = false;
-        } else {
-            x[i] = 0;             // null value if in range
-        }
-    }
-
-    return ok;
-}
-
-void shuffle(const vector<int>& vmin, const vector<int>& vmax, vector<vector<double>> values, vector<int> &results) {
-    for (size_t i = 0; i < values.size(); ++i) {
-        for (size_t j = 0; j < values[i].size(); ++j) {
-            double r = 2.0 * canonical() - 1.0;
-            values[i][j] += 0.4 * r * r * r;
-        }
-    }
-    vector<int> cnt(vmin.size(), 0);
-
-    while (!count(vmin, vmax, cnt, values)) {
-
-        for (size_t i = 0; i < values.size(); ++i) {
-            for (size_t j = 0; j < vmin.size(); ++j) {
-                values[i][j] += 2e-4 * canonical_fast() * cnt[j] * cnt[j] * cnt[j];
-            }
-        }
-    }
-
-    results.resize(values.size());
-    for (size_t i = 0; i < values.size(); ++i)
-        results[i] = min_pos(values[i]);
-}
-
-int action(const vector<vector<double>>& wishes, const vector<int>& results) {
-    int score = 0;
-    for (size_t i = 0; i < wishes.size(); ++i) {
-        score += wishes[i][results[i]] * wishes[i][results[i]];
-    }
-    return score;
-}
-
-vector<int> search_solution(vector<int> vmin, vector<int> vmax, vector<vector<double>> wishes) {
-    int best_score = -1;
-    vector<int> best_results;
-    vector<int> results;
-
-    for (int i = 0; i < 100; ++i) {
-        shuffle(vmin, vmax, wishes, results);
-
-        int score = action(wishes, results);
-
-        if (score < best_score || best_score == -1) {
-            best_score = score;
-            best_results = results;
-        }
-
-        if (g_random_numbers_counter > 8 * g_random_numbers_size)
-            canonical_fast_initialize();
-    }
-
-    return best_results;
-}
 
 pp::VarArray search(const pp::Var& var_message) {
-    const pp::VarDictionary& dic = static_cast<const pp::VarDictionary&>(var_message);
+    pp::VarArray message = static_cast<pp::VarArray>(var_message);
+    size_t n = message.GetLength();
+    cell** cost = (cell**)malloc(n * sizeof(cell*));
 
-    pp::VarArray vmin = static_cast<pp::VarArray>(dic.Get("vmin"));
-    pp::VarArray vmax = static_cast<pp::VarArray>(dic.Get("vmax"));
-    pp::VarArray wishes = static_cast<pp::VarArray>(dic.Get("wishes"));
+    for (size_t i = 0; i < n; ++i) {
+        cost[i] = (cell*)malloc(n * sizeof(cell));
 
-    vector<int> vmin_v(vmin.GetLength());
-    vector<int> vmax_v(vmin.GetLength());
-    vector<vector<double>> wishes_v(wishes.GetLength());
-
-    for (size_t i = 0; i < vmin_v.size(); ++i) {
-        vmin_v[i] = vmin.Get(i).AsInt();
-    }
-    for (size_t i = 0; i < vmax_v.size(); ++i) {
-        vmax_v[i] = vmax.Get(i).AsInt();
-    }
-
-    for (size_t i = 0; i < wishes_v.size(); ++i) {
-        pp::VarArray row = static_cast<pp::VarArray>(wishes.Get(i));
-        vector<double> row_v(row.GetLength());
-        for (size_t j = 0; j < row_v.size(); ++j) {
-            row_v[j] = row.Get(j).AsInt();
+        pp::VarArray row_message = static_cast<pp::VarArray>(message.Get(i));
+        for (size_t j = 0; j < n; ++j) {
+            cost[i][j] = row_message.Get(j).AsInt();
         }
-        wishes_v[i] = row_v;
     }
 
-    vector<int> result_v = search_solution(vmin_v, vmax_v, wishes_v);
+    ssize_t** ass = kuhn_match(cost, n, n);
+
     pp::VarArray result;
-    result.SetLength(result_v.size());
-    for (size_t i = 0; i < result_v.size(); ++i) {
-        result.Set(i, result_v[i]);
+    result.SetLength(n);
+    for (size_t i = 0; i < n; ++i) {
+        result.Set(ass[i][0], ass[i][1]);
     }
+
+    for (size_t i = 0; i < n; ++i) {
+        free(ass[i]);
+        free(cost[i]);
+    }
+    free(ass);
+    free(cost);
+
     return result;
 }
 
